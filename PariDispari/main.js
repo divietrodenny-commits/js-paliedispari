a1ed4799c7a18ca45b2c2327b6d81b202730a640
divietrodenny-commits <divietrodenny@gmail.com>
2026-02-12 18:43:55 +0100

    aggiunto risultato, stampa in console e alert

--- PariDispari/main.js
+++ PariDispari/main.js
@@ -19,3 +19,31 @@ const numPC = generaRandom(1, 5);
 
 /* addizione tra i 2 */
 const somma = numUtente + numPC;
+
+/* inizializzo il risultato */
+let risultato;
+
+/* se la somma è pari, imposto risultato a pari */
+if (pariDispari(somma)) {
+    risultato = 'pari';
+}
+/* altrimenti imposto dispari */
+else {
+    risultato = 'dispari';
+}
+
+/* stampo in console */
+console.log(`tu hai scelto ${sceltaUtente}, numero: ${numUtente}`);
+console.log(`pc: ${numPC}, somma: ${somma}, risultato: ${risultato}`);
+
+/* determino in vincitore, se il risultato è = alla scelta utente stampa hai vinto */
+if (risultato === sceltaUtente) {
+    console.log('Hai vinto');
+    alert('Hai vinto')
+}
+
+/* Altrimenti stampa hai perso */
+else {
+    console.log('Hai perso');
+    alert('Hai perso')
+}
